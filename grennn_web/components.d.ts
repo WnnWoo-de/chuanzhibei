ee04e60aec790d0b89c78cc7edb3b666e9d4b58f
WnnW <1137423303@qq.com>
2026-04-06 14:39:22 +0800

    ci: trigger workers deploy

--- grennn_web/components.d.ts
+++ grennn_web/components.d.ts
@@ -18,7 +18,6 @@ declare module 'vue' {
     ElButton: typeof import('element-plus/es')['ElButton']
     ElCheckbox: typeof import('element-plus/es')['ElCheckbox']
     ElDialog: typeof import('element-plus/es')['ElDialog']
-    ElEmpty: typeof import('element-plus/es')['ElEmpty']
     ElIcon: typeof import('element-plus/es')['ElIcon']
     ElInput: typeof import('element-plus/es')['ElInput']
     ElSkeleton: typeof import('element-plus/es')['ElSkeleton']
